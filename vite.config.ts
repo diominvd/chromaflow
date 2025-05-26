dd9bbc6fa377154f4836c7d1f162df0e4c8f6965
diominvd <diomin.vd13@gmail.com>
2025-05-26 19:55:55 +0300

    build: update the build
    - Update vite configuration
    - Add new scripts in package.json
    - Update .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,48 +4,66 @@ import { resolve } from 'path';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      include: ['lib/**/*'],
-      exclude: ['node_modules/**', 'dist/**'],
-      outDir: 'dist/types'
-    }),
-    libInjectCss()
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      name: 'chromaflow',
-      formats: ['es'],
-      fileName: 'index'
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        assetFileNames: 'index.css',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  if (isDev) {
+    return {
+      plugins: [react()],
+      root: 'test',
+      resolve: {
+        alias: {
+          '@': resolve(__dirname, './lib'),
+          '@react': resolve(__dirname, './lib/frameworks/react')
         }
       }
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
-    sourcemap: false
-  },
-  publicDir: 'lib/public',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './lib'),
-      '@react': resolve(__dirname, './lib/frameworks/react')
-    }
+    };
   }
+
+  return {
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+        include: ['lib/**/*'],
+        exclude: ['node_modules/**', 'dist/**'],
+        outDir: 'dist/types'
+      }),
+      libInjectCss()
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'lib/index.ts'),
+        name: 'chromaflow',
+        formats: ['es'],
+        fileName: 'index'
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom', 'react/jsx-runtime'],
+        output: {
+          assetFileNames: 'index.css',
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM'
+          }
+        }
+      },
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      },
+      sourcemap: false
+    },
+    publicDir: 'lib/public',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './lib'),
+        '@react': resolve(__dirname, './lib/frameworks/react')
+      }
+    }
+  };
 });
